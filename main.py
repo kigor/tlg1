6a055f5c460ce2e8cfa51db22be1b9e0d4e7263d
Igor K <kripinevich.i@gmail.com>
2024-08-31 23:59:01 +0800

    change 2 cats

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ API_URL = 'https://api.telegram.org/bot'
 BOT_TOKEN = '7351163387:AAEXCihHtqh1DFyRy081K22qBkSsVb0op-I'
 TEXT = 'Ура! Классный апдейт!'
 MAX_COUNTER = 100
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
+
 
 offset = -2
 counter = 0
 chat_id: int
+cat_response: requests.Response
+cat_link: str
 
 
 while counter < MAX_COUNTER:
 
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+    print('attempt =', counter)  # Чтобы видеть в консоли, что код живет
 
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
@@ -22,7 +27,11 @@ while counter < MAX_COUNTER:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
-
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
     time.sleep(1)
     counter += 1
