975497cd391b87a177480a6a84bc72aa339013e3
Igor K <kripinevich.i@gmail.com>
2024-09-01 14:40:14 +0800

    echo_done2

--- 1_echo.py
+++ 1_echo.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types import ContentType
+from aiogram import F
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
 BOT_TOKEN = '7351163387:AAEXCihHtqh1DFyRy081K22qBkSsVb0op-I'
@@ -24,6 +26,19 @@ async def process_help_command(message: Message):
         'я пришлю тебе твое сообщение'
     )
 
+# Этот хэндлер будет срабатывать на отправку боту фото
+# dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
+# @dp.message(F.content_type == ContentType.PHOTO)
+@dp.message(F.photo)
+async def send_photo_echo(message: Message):
+    print(message)
+    await message.reply_photo(message.photo[0].file_id)
+
+@dp.message(F.sticker)
+async def send_photo_echo(message: Message):
+    print(message)
+    await message.reply_sticker(message.sticker.file_id)
+
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
@@ -31,6 +46,8 @@ async def process_help_command(message: Message):
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
+#    await bot.send_message(chat_id='ID или название чата', text='Какой-то текст')
+#    await bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
