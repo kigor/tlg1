0f6e95691bd168404d6d258986638f999b96934d
Igor K <kripinevich.i@gmail.com>
2025-03-24 12:48:32 +0800

    new_commit_240325

--- 7_filter.py
+++ 7_filter.py
@@ -11,7 +11,7 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 # Список с ID администраторов бота. !!!Замените на ваш!!!
-admin_ids: list[int] = [2022050770]
+admin_ids: list[int] = [202205077]
 
 
 # Собственный фильтр, проверяющий юзера на админа
@@ -24,16 +24,16 @@ class IsAdmin(BaseFilter):
         return message.from_user.id in self.admin_ids
 
 # # Этот хэндлер будет срабатывать, если апдейт от админа
-# @dp.message(IsAdmin(admin_ids))
-# async def answer_if_admins_update(message: Message):
-#     await message.answer(text='Вы админ')
+@dp.message(IsAdmin(admin_ids))
+async def answer_if_admins_update(message: Message):
+    await message.answer(text='Вы админ ' + str(message.from_user.id))
 
-83832359513
+#83832359513
 
 # # Этот хэндлер будет срабатывать, если апдейт не от админа
-# @dp.message()
-# async def answer_if_not_admins_update(message: Message):
-#     await message.answer(text='Вы не админ')
+@dp.message()
+async def answer_if_not_admins_update(message: Message):
+    await message.answer(text='Вы не админ ' + str(message.from_user.id))
 
 
 # Этот фильтр будет проверять наличие неотрицательных чисел
